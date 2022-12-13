9ea255da9fbaf53cc7645df1916b109e52ee5845
janvacek <hvacek22@gmail.com>
2022-12-13 14:37:58 +0100

    QR PREVIEW 1

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>JDEME ZKUSIT PRVNÍ DEPLOY</Text>
+      <Text>TESTUJEME</Text>
+      <Text>TESTUJEME</Text>
       <Text>TESTUJEME</Text>
       <Text>TESTUJEME</Text>
       <Text>THIS IS A STAGING! TEST 1</Text>
