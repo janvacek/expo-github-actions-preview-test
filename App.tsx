9de3594c7ffc5276519bb9e50bbb309c256281a3
janvacek <hvacek22@gmail.com>
2022-12-13 12:14:50 +0100

    test 1 1

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>THIS IS A STAGING!</Text>
+      <Text>THIS IS A STAGING! TEST 1</Text>
       <StatusBar style="auto" />
     </View>
   );
